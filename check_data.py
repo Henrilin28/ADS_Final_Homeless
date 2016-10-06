1baf4593ee3145d5ef8bbccf1d649b9d6709bb82
Henry lin <imhenry@me.com>
2016-10-06 16:10:50 -0400

    update metadata

--- check_data.py
+++ check_data.py
@@ -3,15 +3,25 @@ import urllib
 import pandas as pd
 import numpy as np
 
+
+def read_date (filelist):
+    file_link = []
+    with open(filelist,"r") as f:
+        
+        for idx, line in enumerate(f):
+            if idx !=0:
+                 a = line.strip("\n").split(" ")
+                 file_link.append(a)
+
+    return file_link
 def check_data (filename,link):
 
     df = pd.read_csv(link)
-
+    print ("{} download complete".format(filename))
     with open(filename+"metadata.txt","w") as f:
         for column in df.columns.values:
             f.write(column)
             f.write("\n")
-            print (df[column].unique())
             if df[column].dtype == "object" : 
                 f.write("unique_value: \n")   
                 f.write("/".join(str(val) for val in df[column].unique()))
@@ -20,7 +30,15 @@ def check_data (filename,link):
                 f.write("    numerical")
                 f.write("\n")
         f.write(str(df.shape))
-
+    print ("{} metadata complete".format(filename))
 if __name__ == '__main__':
 
-    check_data(sys.argv[2],sys.argv[1])
+    index = 0 
+    file_link = read_date (sys.argv[1])
+
+    while(index<len(file_link)):
+        temp = file_link[index]
+        check_data (temp[0],temp[1])
+
+        index +=1
+
